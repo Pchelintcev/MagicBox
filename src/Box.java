8c3c2715b67788a12d989d8acf6bb9cfd3be34ad
Сергей Пчелинцев <s.pchelintcev@yandex.ru>
2022-08-11 11:42:59 +0300

    добавлен случайный элемент

--- src/Box.java
+++ src/Box.java
@@ -1,38 +1,40 @@
-public class Box<T> {
-    protected int size;
-    protected T[] items;
+import java.util.Arrays;
+import java.util.Random;
 
-    //TODO конструктор коробок:
+public class Box<T> {
+    protected int size; //размер
+    protected  T[] items;
+
     public Box(int size) {
         this.size = size;
         items = (T[]) new Object[size];
     }
 
-    //TODO метод добавления объекта:
+    Random random = new Random();
+    int filled = 0; // занятые ячейки
+
     public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 items[i] = item;
+                filled += 1;
+                System.out.println(Arrays.toString(items));
+                System.out.println("свободных ячеек: " + (items.length - filled));
+                if (filled == items.length){
+                    int randomInt = random.nextInt(items.length);
+                    System.out.println("случайный элемент из коробки: " + items[randomInt]);
+                    System.out.println();
+                }
                 return true;
             }
         }
+        System.out.println("коробка полна, нет места для добавления нового объекта.");
         return false;
     }
 }
 
 
-//TODO проверка на null:
-
-//    for (int i = 0; i < items.length; i++) {
-//        if (items[i] == null) {
-//            System.out.println("коробка ещё не наполнена, осталось: " + "несколько свободных ячеек.");
-//            continue;
-//        }
-//    }
 
 
-//        public static int getRandom (T item){
-//            Random random = new Random();
-//            int randomInt = random.nextInt(items.length) + 1;
-//        }
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
-        //TODO создаем объект класса Box:
-        Box <Integer> box1 = new Box<>( 5);
-        box1.add(15);
-        box1.add(45);
-        box1.add(4);
-        box1.add(18);
-        box1.add(1);
-        box1.add(2);
+        Box <String> box1 = new Box <>(2);
+        Box <Integer> box2 = new Box<>( 3);
 
-        Box <String> box2 = new Box <>(2);
-        box2.add("table");
 
-        System.out.println(Arrays.toString(box1.items));
-        System.out.println(Arrays.toString(box2.items));
+        box1.add("table");
+        box1.add("chair");
+
+        box2.add(15);
+        box2.add(45);
+        box2.add(4);
+
+        box2.add(18);
+
+
+
     }
 }
